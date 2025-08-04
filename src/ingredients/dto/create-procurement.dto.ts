b8fe555caa48cd99f8582d4b75bbc1a73219c295
aleigood <aleigood@gmail.com>
2025-08-05 00:27:39 +0800

    完善原料页面

--- src/ingredients/dto/create-procurement.dto.ts
+++ src/ingredients/dto/create-procurement.dto.ts
@@ -1,10 +1,6 @@
 import { IsDateString, IsInt, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateProcurementDto {
-    @IsString()
-    @IsNotEmpty()
-    skuId: string;
-
     @IsInt()
     @IsNotEmpty()
     packagesPurchased: number;
