1bd67abb5964d15692b9e3191fdc11c0ee30731f
aleigood <aleigood@gmail.com>
2025-11-04 22:02:55 +0800

    取消自动执行脚本，防止自动应用数据库迁移

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for database to be ready..."
-sleep 10
+# echo "Waiting for database to be ready..."
+# sleep 10
 
-echo "Running database migrations..."
-npx prisma migrate deploy
+# echo "Running database migrations..."
+# npx prisma migrate deploy
 
-echo "Running database seed..."
-node dist/prisma/seed.js || { echo '[SEED] Seed script failed'; exit 1; }
+# echo "Running database seed..."
+# node dist/prisma/seed.js || { echo '[SEED] Seed script failed'; exit 1; }
 
 echo "Starting application..."
 node dist/main
